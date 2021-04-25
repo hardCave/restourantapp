adf0fa007525072954aa4e8bd2cffc85a5cc834c
Bahadır Berk <browntoclown@gmail.com>
2021-04-25 20:36:24 +0300

    main page - page cards added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,10 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
+    var cardTextStyle = TextStyle(
+        fontFamily: "Raleway",
+        fontSize: 14,
+        color: Color.fromRGBO(63, 63, 63, 1));
     return Scaffold(
       body: Stack(
         children: <Widget>[
@@ -20,6 +24,152 @@ class _HomeScreenState extends State<HomeScreen> {
                     alignment: Alignment.topCenter,
                     image: AssetImage('assets/images/top_header.png'))),
           ),
+          GridView.count(
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            crossAxisCount: 2,
+            primary: false,
+            children: <Widget>[
+              Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(31)),
+                  elevation: 4,
+                  margin: const EdgeInsets.only(
+                    left: 15.0,
+                    right: 15.0,
+                    top: 15.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.add_moderator,
+                        color: Colors.deepOrange,
+                        size: 90.0,
+                      ),
+                      Text(
+                        'SİPARİŞ AL ',
+                        style: cardTextStyle,
+                      ),
+                    ],
+                  )),
+              Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(31)),
+                  elevation: 4,
+                  margin: const EdgeInsets.only(
+                    left: 15.0,
+                    right: 15.0,
+                    top: 15.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.add_shopping_cart,
+                        color: Colors.deepOrange,
+                        size: 90.0,
+                      ),
+                      Text(
+                        'ÜRÜN EKLEME',
+                        style: cardTextStyle,
+                      )
+                    ],
+                  )),
+              Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(31)),
+                  elevation: 4,
+                  margin: const EdgeInsets.only(
+                    left: 15.0,
+                    right: 15.0,
+                    top: 15.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.add_alert,
+                        color: Colors.deepOrange,
+                        size: 90.0,
+                      ),
+                      Text(
+                        'GÜN SONU',
+                        style: cardTextStyle,
+                      )
+                    ],
+                  )),
+              Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(31)),
+                  elevation: 4,
+                  margin: const EdgeInsets.only(
+                    left: 15.0,
+                    right: 15.0,
+                    top: 15.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.add_moderator,
+                        color: Colors.deepOrange,
+                        size: 90.0,
+                      ),
+                      Text(
+                        'SİPARİŞ AL ',
+                        style: cardTextStyle,
+                      )
+                    ],
+                  )),
+              Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(31)),
+                  elevation: 4,
+                  margin: const EdgeInsets.only(
+                    left: 15.0,
+                    right: 15.0,
+                    top: 15.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.add_moderator,
+                        color: Colors.deepOrange,
+                        size: 90.0,
+                      ),
+                      Text(
+                        'SİPARİŞ AL ',
+                        style: cardTextStyle,
+                      )
+                    ],
+                  )),
+              Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(31)),
+                  elevation: 4,
+                  margin: const EdgeInsets.only(
+                    left: 15.0,
+                    right: 15.0,
+                    top: 15.0,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.add_moderator,
+                        color: Colors.deepOrange,
+                        size: 90.0,
+                      ),
+                      Text(
+                        'SİPARİŞ AL ',
+                        style: cardTextStyle,
+                      )
+                    ],
+                  ))
+            ],
+          )
         ],
       ),
     );
