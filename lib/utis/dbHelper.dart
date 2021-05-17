2ffdcd184d1e05ddb39a58238d57c982225c6d0c
Talha <yucelltalha@gmail.com>
2021-05-17 04:12:45 +0300

    backend

--- lib/utis/dbHelper.dart
+++ lib/utis/dbHelper.dart
@@ -193,6 +193,18 @@ class DatabaseHelper {
         await db.delete("ReportZ", where: 'ID = ?', whereArgs: [productId]);
     return result;
   }
+  Future<int> deleteZAll() async {
+    Database db = await _getDatabase();
+    var result =
+    await db.delete("ReportZ");
+    return result;
+  }
+  Future<int> deleteZbyDay(int year,int month,int day) async {
+    Database db = await _getDatabase();
+    var result =
+    await db.delete("ReportZ", where: "dateYear=$year AND dateMonth=$month AND dateDay=$day");
+    return result;
+  }
 
   Future<int> updateZ(ReportZ z) async {
     Database db = await _getDatabase();
@@ -241,6 +253,11 @@ class DatabaseHelper {
     Database db = await _getDatabase();
     var result = await db.update("daily", d.toJson(),
         where: 'ID = ?', whereArgs: [d.id]);
+    return result;
+  }
+  Future<int> insertDailySimple(DailyProd d) async {
+    Database db = await _getDatabase();
+    var result = await db.insert("daily", d.toJson());
     return result;
   }
 //-------------------------------------------------------DAILY SECTION-------------------------------------//
