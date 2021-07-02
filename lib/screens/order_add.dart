b7144d577c27291d45d631172144c85a4ac8a96b
Talha <yucelltalha@gmail.com>
2021-07-02 16:43:49 +0300

    backend

--- lib/screens/order_add.dart
+++ lib/screens/order_add.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite_demo/models/currentProducts.dart';
 import 'package:sqflite_demo/models/dailyProds.dart';
 import 'package:sqflite_demo/models/productModel.dart';
 import 'package:sqflite_demo/models/tablesModel.dart';
@@ -104,7 +105,7 @@ class _OrderAddState extends State {
                   margin: EdgeInsets.all(10),
                   //color: Colors.purple,
                   width: 240,
-                  height: 630,
+                  height: 500,
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
@@ -119,7 +120,7 @@ class _OrderAddState extends State {
                   children: [
                     Container(
                       width: 160,
-                      height: 630,
+                      height: 500,
                       color: Colors.orange.shade200,
                       child: Column(
                         children: [
@@ -294,18 +295,20 @@ class _OrderAddState extends State {
           }),
     );
   }
-  var dList = List<DailyProd>();
+
   void dailyCreate(int massa) async {
     var liste = await dbhelper.getTableList();
     var daily = await dbhelper.getDailyList();
     var defProds = await dbhelper.getProdList();
-    List currentTable;
+    var currentTable = List();
+    var pmap = List<List>();
     var counttt = 0;
     var dailyId = 0;
-
+    List<DailyProd> dList = [];
 
     //default ürün listesini oluştur
-       if(dList.isEmpty) {
+
+    if (dList.isEmpty) {
       for (int i = 0; i < defProds.length; i++) {
         var dd = DailyProd();
         dd.id = null;
@@ -315,15 +318,17 @@ class _OrderAddState extends State {
       }
     }
 
+
     //default ürün listesini mevcut ürünler varsa güncelle
-    for(int j = 0;j<dList.length;j++){
+    for (int j = 0; j < dList.length; j++) {
       for (int i = 0; i < daily.length; i++) {
-        if (daily[i].productId==dList[j].productId) {
+        if (daily[i].productId == dList[j].productId) {
           dList[j].productsCount = daily[i].productsCount;
         }
       }
     }
 
+    dbhelper.deleteTableDaily();
 
     //ilgili masayı çek ve sırala
     for (int i = 0; i < liste.length; i++) {
@@ -331,38 +336,57 @@ class _OrderAddState extends State {
         currentTable = liste[i].tableProducts;
       }
     }
-    currentTable.sort();
-    //masadaki ürünlerin kaçar tane olduğunu bul
-    int a = currentTable[0];
-    print(currentTable);
+
+    //default map olustur
+    for(int i = 0;i<defProds.length;i++){
+      var list = List();
+      list.add(defProds[i].productId);
+      list.add(0);
+      pmap.add(list);
+    }
+
+    for(int i = 0;i<currentTable.length;i++){
+      for(int j = 0;j<pmap.length;j++){
+        if (currentTable[i] == pmap[j][0]){
+          pmap[j][1]++;
+        }
+      }
+    }
+
+    for(int i = 0;i<pmap.length;i++){
+      if(dList[i].productId == pmap[i][0]){
+        dList[i].productsCount += pmap[i][1];
+      }
+    }
+
+    for (int i = 0; i < dList.length; i++) {
+      dbhelper.insertDaily(dList[i]);
+    }
+/*
+     // masadaki ürünlerin kaçar tane olduğunu bul
+    int a = currentTable.first;
     for (int i = 0; i < currentTable.length; i++) {
       if (currentTable[i] == a) {
         counttt++;
         dailyId = currentTable[0];
       } else {
-        a = currentTable[i];
-        var d = DailyProd();
-        d.productId = dailyId;
-        d.productsCount = counttt;
-        d.id = null;
-        //ürünleri adediyle beraber default listeye ekle
-        for(int a = 0;a<dList.length;a++){
-          if(dList[a].productId==d.productId){
-            dList[a].productsCount += d.productsCount;
+        //ürünleri adediyle beraber default listeye eşitle
+        for (int x = 0; x < dList.length; x++) {
+          if (dList[x].productId == dailyId) {
+            dList[x].productsCount += counttt;
           }
         }
-
+        a = currentTable[i];
         counttt = 1;
       }
     }
+    currentTable.clear();
     //listedeki ürünleri tek tek veritabanına aktar
 
-    for(int i = 0;i<dList.length;i++){
+    for (int i = 0; i < dList.length; i++) {
       dbhelper.insertDaily(dList[i]);
-    }
-
+    }*/
   }
-
 
   /*addedOrder() {
     return FutureBuilder<List<ProductsTable>>(
